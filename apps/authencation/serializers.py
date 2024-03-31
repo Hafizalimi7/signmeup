267eed8ca93ce16606395575049530c183c1deb8
Hafiz <hafizalimi77@gmail.com>
2024-03-31 16:04:16 +0100

    password now hashes into db

--- apps/authencation/serializers.py
+++ apps/authencation/serializers.py
@@ -1,16 +1,24 @@
 from rest_framework import serializers
 from .models import User
+from django.contrib.auth.hashers import make_password
 
 class RegisterSerializer(serializers.ModelSerializer):
+  password = serializers.CharField(
+        style={'input_type': 'password'}
+    )
   class Meta:
     model = User
     fields = ['first_name', 'last_name', 'username','email', 'password','date_of_birth']
+    extra_kwargs = {'password': {'write_only': True}}
     
-    def create(self, validated_data):
-      user = User.objects.create(
-        first_name = validated_data['first_name'],
-        last_name = validated_data['last_name'],
-        username = validated_data['username'],
-      )
-      user.save()
-      return user
+  def create(self, validated_data):
+    user = User.objects.create(
+      first_name = validated_data['first_name'],
+      last_name = validated_data['last_name'],
+      username = validated_data['username'],
+      email = validated_data['email'],
+      date_of_birth = validated_data['date_of_birth']
+    )
+    user.set_password(validated_data['password'])
+    user.save()
+    return user
